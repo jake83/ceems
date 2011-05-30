79cc214b6c58d1526f2e84c89b3d8113c11b453b
Jacob Watson <jwatson1983@gmail.com>
2011-05-30 21:06:10 +0100

    Started work on paging system

--- classes/page.php
+++ classes/page.php
@@ -4,14 +4,18 @@ class Page {
 
 	private $page;
 
-	private function get_page_by_pagenum($pagenum) {
+	private function get_page_by_pagenum($pagenum, $pageorder) {
 		global $db;
-		$query = "SELECT * ";
-		$query .= "FROM pages ";
-		$query .= "WHERE pagenum=" . $pagenum ." ";
-		$query .= "LIMIT 1";
-		$result_set = $db->query($query);
 		
+		$query  = "SELECT pages.name, pages.content, pages.pageorder ";
+		$query .= "FROM pages ";
+		$query .= "INNER JOIN sections ";
+		$query .= "ON pages.pagenum = sections.pagenum ";
+		$query .= "WHERE sections.pagenum=". $pagenum;
+		$query .= " AND sections.enabled=1 ";
+		$query .= "AND pages.enabled=1";
+		
+		$result_set = $db->query($query);		
 		if ($this->page = $db->fetch_array($result_set)) {
 			return $this->page;
 		} else {
@@ -21,8 +25,8 @@ class Page {
 
 	public function find_selected_page() {
 		global $sel_page;
-		if (isset($_GET['page'])) {
-			$sel_page = $this->get_page_by_pagenum($_GET['page']);
+		if (isset($_GET['page']) && isset($_GET['pageorder'])) {
+			$sel_page = $this->get_page_by_pagenum($_GET['page'], $_GET['pageorder']);
 		} else {
 			$sel_page = NULL;
 		}
