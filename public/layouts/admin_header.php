f3f46b0b25010e5f84141ff7e5983e7d0f574777
Jacob Watson <jwatson1983@gmail.com>
2011-06-04 23:45:44 +0100

    Started work on back office. Added ability to insert pages and sections

---
+++ public/layouts/admin_header.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>CeeMS: Admin</title>
+		
+		<link rel="stylesheet" type="text/css" href="../stylesheets/reset.css" />
+		<link rel="stylesheet" type="text/css" href="../stylesheets/page.css" />
+		<link rel="stylesheet" type="text/css" href="../stylesheets/general.css" />
+		<link rel="stylesheet" type="text/css" href="../stylesheets/grid.css" />
+	</head>
+	<body>
+		<div id="header">
+			<h1>CeeMS: Admin</h1>
+		</div>
+		<div id="main">
