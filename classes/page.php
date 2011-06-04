182a52ba9832c67b24aa3e4f72818e2880384366
Jacob Watson <jwatson1983@gmail.com>
2011-06-04 23:40:32 +0100

    Started work on back office. Added ability to insert pages and sections

--- classes/page.php
+++ classes/page.php
@@ -2,13 +2,42 @@
 
 class Page {
 
+	public $pagenum;
+	public $pageorder;
+	public $pagename;
+	public $content;
+	public $enabled;
+	public $footerpromo;
 	private $page;
 	private $pagecount = array();
 
+	function __construct() {
+		if(isset($_POST['pagenum'])) { $this->pagenum = $_POST['pagenum']; }
+		if(isset($_POST['pageorder'])) { $this->pageorder = $_POST['pageorder']; }
+		if(isset($_POST['name'])) { $this->pagename = $_POST['name']; }
+		if(isset($_POST['content'])) { $this->content = $_POST['content']; }
+		if(isset($_POST['page_enabled'])) { $this->enabled = $_POST['page_enabled']; }
+		if(isset($_POST['footerpromo'])) { $this->footerpromo = $_POST['footerpromo']; }
+	}
+	
+	public function insert_page() {
+		global $db;
+		$query  = "INSERT INTO pages ";
+		$query .= "(pagenum, pageorder, name, content, enabled) ";
+		$query .= "VALUES (";
+		$query .= $this->pagenum .", ";
+		$query .= $this->pageorder .", ";
+		$query .= "'". $this->pagename ."', ";
+		$query .= "'". $this->content ."', ";
+		$query .= $this->enabled ."', ";
+		$query .= "'". $this->footerpromo ."')";
+		$db->query($query);
+	}
+	
 	private function get_page_by_pagenum($pagenum, $pageorder) {
 		global $db;
 		
-		$query  = "SELECT pages.name, pages.content, pages.pageorder ";
+		$query  = "SELECT pages.name, pages.content, pages.pageorder, pages.footerpromo ";
 		$query .= "FROM pages ";
 		$query .= "INNER JOIN sections ";
 		$query .= "ON pages.pagenum = sections.pagenum ";
