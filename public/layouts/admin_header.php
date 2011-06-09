8d0f292316dbcbc35b56eb08ac1750936f5d8335
Jacob Watson <jwatson1983@gmail.com>
2011-06-09 22:00:56 +0100

    Added some styling bits

--- public/layouts/admin_header.php
+++ public/layouts/admin_header.php
@@ -1,14 +1,32 @@
+<!DOCTYPE html>
+
 <html>
-	<head>
-		<title>CeeMS: Admin</title>
+<head>
+<title>CeeMS: Admin</title>	
+<link rel="stylesheet" type="text/css" href="../stylesheets/reset.css" />
+<link rel="stylesheet" type="text/css" href="../stylesheets/admin.css" />
+<link rel="stylesheet" type="text/css" href="../stylesheets/general.css" />
+<link rel="stylesheet" type="text/css" href="../stylesheets/grid.css" />
+</head>
+<body>
+<div id="admin_top">
+	<header>
+		<nav>
+			<ul>
+				<li class="grid_4" id="headersearch"><input type="text" size="3" /></li>
+				<li class="grid_4" id="headertitle"><div>CeeMS<div></li>
+				<li class="grid_4" id="headernum"><div>100</div></li>
+				<li class="grid_4" id="headerdate"><div></div></li>
+				<li class="grid_4" id="headertime"><div></div></li>
+			</ul>
+		</nav>
 		
-		<link rel="stylesheet" type="text/css" href="../stylesheets/reset.css" />
-		<link rel="stylesheet" type="text/css" href="../stylesheets/page.css" />
-		<link rel="stylesheet" type="text/css" href="../stylesheets/general.css" />
-		<link rel="stylesheet" type="text/css" href="../stylesheets/grid.css" />
-	</head>
-	<body>
-		<div id="header">
-			<h1>CeeMS: Admin</h1>
-		</div>
-		<div id="main">
+		<header>
+			<hgroup class="grid_20" id="headerintro">
+				<h1>CeeMS Admin</h1>
+			</hgroup>
+		</header>
+
+	<h1>CeeMS: Admin</h1>
+</div>
+<div id="admin_main">
